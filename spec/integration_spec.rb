2e6655fae55730435dfaf07ca339998bfd059aca
Vaughn Dice <donvice@gmail.com>
2015-08-21 11:32:38 -0700

    add tests for presenting lists of all stylists and/or all clients

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -12,11 +12,25 @@ describe('The Hair Salon Management App', {:type => :feature}) do
       expect(page).to have_content('Manage Stylists')
       expect(page).to have_content('It looks like you don\'t have any stylists!')
     end
+    it('presents the user with a listing of all stylists') do
+      @stylist.save()
+      visit('/stylists')
+      expect(page).to have_content(@stylist.name())
+    end
+  end
+
+  describe('the client management path') do
     it('takes the user to the modifiable list of clients, empty at first') do
       visit('/')
       click_link('View, add, update and delete clients')
       expect(page).to have_content('Manage Clients')
       expect(page).to have_content('It looks like you don\'t have any clients!')
+    end
+    it('presents the user with a listing of all stylists') do
+      @stylist.save()
+      @client.save()
+      visit('/clients')
+      expect(page).to have_content(@client.name())
     end
   end
 end
