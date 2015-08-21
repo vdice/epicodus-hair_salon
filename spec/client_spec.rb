ed735957680ffe2fd9b0f7f0e8a6f8eda92f9fa5
Vaughn Dice <donvice@gmail.com>
2015-08-21 09:56:38 -0700

    add implementation to pass stylist method for Client instances

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,14 +2,20 @@ require('spec_helper')
 
 describe(Client) do
   describe('#name') do
-    it('returns the name of the client') do
+    it('returns the client\'s name') do
       expect(@client.name()).to(eq('Bucephalus'))
     end
   end
 
   describe('#id') do
-    it('returns the id of the client') do
+    it('returns the client\'s id') do
       expect(@client.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#stylist') do
+    it('returns the client\'s stylist') do
+      expect(@client.stylist()).to(eq(@stylist))
     end
   end
 end
